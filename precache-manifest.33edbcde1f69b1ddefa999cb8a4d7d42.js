c364b6778c6c9977448d4f15608ac0bbfec9a260
unknown <ittalentsvahagn@gmail.com>
2020-05-28 21:12:33 +0400

    Updates

---
+++ precache-manifest.33edbcde1f69b1ddefa999cb8a4d7d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abedb574d39e7f2442873e2b1e19d352",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "f3858005947b7de130ad",
+    "url": "/notes-app/static/css/main.eebf778e.chunk.css"
+  },
+  {
+    "revision": "492965f274400284bee7",
+    "url": "/notes-app/static/js/2.7f4d7683.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notes-app/static/js/2.7f4d7683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3858005947b7de130ad",
+    "url": "/notes-app/static/js/main.bbe4c3b5.chunk.js"
+  },
+  {
+    "revision": "75a2ac30e9ab7e6b9722",
+    "url": "/notes-app/static/js/runtime-main.342582bc.js"
+  }
+]);
